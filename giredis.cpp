18c1053a84d972bd2d17aa2480d563209d9dea01
Abhi Agarwal <me@abhiagarwal.com>
2016-02-22 18:16:00 -0500

    Fixing cmake

--- giredis.cpp
+++ giredis.cpp
@@ -1,10 +1,22 @@
+#include <cpp_redis/cpp_redis>
+
 #include <algorithm>
 #include <iostream>
 #include <cstring>
 #include <stdio.h>
 #include <string.h>
 #include <vector>
+#include <signal.h>
 using namespace std;
+
+volatile std::atomic_bool should_exit(false);
+cpp_redis::redis_client client;
+
+void
+sigint_handler(int) {
+    std::cout << "disconnected (sigint handler)" << std::endl;
+    client.disconnect();
+}
 
 int main(int argc, char const *argv[])
 {
