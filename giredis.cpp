6c5156078e8a239b27f97f8ab6cfc6f05f86b77a
Abhi Agarwal <me@abhiagarwal.com>
2016-02-22 17:54:09 -0500

    Initial commit

---
+++ giredis.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    printf("X");
+    return 0;
+}
