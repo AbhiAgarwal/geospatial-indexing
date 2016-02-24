71598fa66d647ae2d370414826bd539947690f5c
Abhi Agarwal <me@abhiagarwal.com>
2016-02-24 17:13:42 -0500

    C++14 and small syntax changes

--- giredis.cpp
+++ giredis.cpp
@@ -1,6 +1,6 @@
 #include <cpp_redis/cpp_redis>
 
-#include <signal.h>
+#include <csignal>
 #include <iostream>
 
 volatile std::atomic_bool should_exit(false);
@@ -11,31 +11,38 @@ void sigint_handler(int) {
     client.disconnect();
 }
 
-void geoadd(std::string city, std::string latitude, std::string longitude, std::string area) {
-    client.send({"GEOADD", city, latitude, longitude, area}, [] (cpp_redis::reply& reply) {
+void geoadd(const std::string& city, const std::string& latitude, const std::string& longitude, const std::string& area) {
+    client.send({"GEOADD", city, latitude, longitude, area}, [] (auto reply) {
         std::cout << reply.as_string() << std::endl;
     });
 }
 
-void geodist(std::string city, std::string location_one, std::string location_two) {
-    client.send({"GEODIST", city, location_one, location_two}, [] (cpp_redis::reply& reply) {
+void geodist(const std::string& city, const std::string& location_one, const std::string& location_two) {
+    client.send({"GEODIST", city, location_one, location_two}, [] (auto reply) {
         std::cout << reply.as_string() << std::endl;
     });
 }
 
-void geohash(std::string city, std::string location_one) {
-    client.send({"GEOHASH", city, location_one}, [] (cpp_redis::reply& reply) {
+void geohash(const std::string& city, const std::string& location_one) {
+    client.send({"GEOHASH", city, location_one}, [] (auto reply) {
         std::cout << reply.as_string() << std::endl;
     });
 }
 
-int main(void) {
-    client.set_disconnection_handler([] (cpp_redis::redis_client&) {
+void inputTestData() {
+    geoadd("nyc", "40.7362722", "-73.9783613", "apartment");
+    geoadd("nyc", "40.7475228", "-73.945493", "lic market");
+}
+
+int main() {
+    client.set_disconnection_handler([] (auto&) {
         std::cout << "client disconnected (disconnection handler)" << std::endl;
         should_exit = true;
     });
 
     client.connect();
+
+    inputTestData();
 
     signal(SIGINT, &sigint_handler);
     while (not should_exit);
